83b17d7d99d0fe746ff77caaf6c64a88c6e67fd0
Braian-92 <zamudiobraianhernan@gmail.com>
2023-05-14 02:12:18 -0300

    eventos con panel de acciones

--- index03.php
+++ index03.php
@@ -16,8 +16,8 @@
     .wrapper .content-wrapper {
       min-height: 100vh;
     }
-    #map{
-    	overflow: hidden;
+    .ovHid{
+      overflow: hidden;
     }
   </style>
 </head>
@@ -27,16 +27,55 @@
     <div class="content-wrapper kanban" style="margin-left: 0!important">
       <section class="content p-2">
         <div class="row h-100">
-          <div class="col-12 h-100">
+          <div class="col-3 h-100">
+            <div class="card card-row card-default w-100 h-100">
+              <div id="listado" class="card-body p-0 m-0 h-100 ovHid">
+                <div class="row">
+                  <div class="col-12 py-1 px-3">
+                    <h5 class="text-info">Configuración</h5>
+                    <hr class="bg-white p-0 m-0">
+                    <ul class="nav flex-column">
+                      <li class="nav-item">
+                        <a href="#" class="nav-link">
+                          Zoom <span id="zoom-nivel" class="float-right badge bg-primary">0</span>
+                        </a>
+                      </li>
+                      <li class="nav-item">
+                        <a href="#" class="nav-link">
+                          Centro mapa <span id="centro-mapa-lng" class="float-right badge bg-info">5</span><span id="centro-mapa-lat" class="float-right badge bg-info mr-1">5</span>
+                        </a>
+                      </li>
+                      <li class="nav-item">
+                        <a href="#" class="nav-link">
+                          Centro mouse <span id="centro-mouse-lng" class="float-right badge bg-info">5</span><span id="centro-mouse-lat" class="float-right badge bg-info mr-1">5</span>
+                        </a>
+                      </li>
+                      <li class="nav-item">
+                        <button type="button" id="geolocalizar" class="btn btn-primary btn-block mt-2">
+                          Geolocalizar
+                        </button>
+                      </li>
+                      <li class="nav-item" style="display: none;">
+                        <button type="button" id="geolocalizar-zoom" class="btn btn-primary btn-block mt-2">
+                          Zoom geolocalizar
+                        </button>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>            
+          </div>
+          <div class="col-9 h-100">
             <div class="card card-row card-default w-100 h-100">
               <div class="card-header bg-olive  d-none">
                 <h3 class="card-title">
-                	<i class="nav-icon fas fa-laptop-house"></i>
+                  <i class="nav-icon fas fa-laptop-house"></i>
                   Nueva
                 </h3>
                
               </div>
-              <div id="map" class="card-body p-0 m-0 h-100">
+              <div id="map" class="card-body p-0 m-0 h-100 ovHid">
                 
               </div>
             </div>            
@@ -61,6 +100,7 @@
     // maxZoom : 5,
   });
   let miPos;
+  let miPosGeo;
   L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);
 
   setInterval(function (){
@@ -74,6 +114,7 @@
   });
   map.on("zoomend", function () {
      console.log('getZoom', map.getZoom());
+     setearZoom();
   });
 
   map.on("click", function (e) {
@@ -114,13 +155,14 @@
     //   color  : '#00ffff',
     //   opacity: 0.75,
     // }).addTo(map);
-
+    var curtime = new Date();
+    miPosGeo = e.latlng;
     miPos = L.circle(e.latlng, {
       radius : e.accuracy/2,
       color  : '#00ffff',
       opacity: 0.75,
     }).addTo(map);
-    alert('usted fue geolocalizado')
+    // alert('usted fue geolocalizado')
 
     L.marker(e.latlng, {icon: L.AwesomeMarkers.icon({icon: 'star', prefix: 'fa', markerColor: 'red', spin:false}) })
     .addTo(map)
@@ -130,6 +172,43 @@
 
   map.on("locationerror", function (e) {
     console.log('mapa no funciona');
+  });
+
+  setearZoom();
+  function setearZoom(){
+    $('#zoom-nivel').text(map.getZoom());
+  }
+  // centro-mapa
+  // mause-geo
+
+  map.on("moveend", function (e) {
+    actualizarCentroMapa();
+  });
+
+  actualizarCentroMapa();
+  function actualizarCentroMapa(){
+    console.log('centroMapa', map.getCenter())
+    $('#centro-mapa-lat').text('LAT: '+ map.getCenter().lat.toFixed(1));
+    $('#centro-mapa-lng').text('LONG: '+ map.getCenter().lng.toFixed(1));
+  }
+
+  map.on("mousemove", function (e) {
+    actualizarPosMause(e);
+    console.log('mouseMove');
+  });
+
+  function actualizarPosMause(evento){
+    $('#centro-mouse-lat').text('LAT: '+ evento.latlng.lat.toFixed(1));
+    $('#centro-mouse-lng').text('LONG: '+ evento.latlng.lng.toFixed(1));
+  }
+
+  $(document).on('click', '#geolocalizar', function(e){
+    map.locate();
+    $('#geolocalizar-zoom').parent().show();
+  });
+
+  $(document).on('click', '#geolocalizar-zoom', function(e){
+    map.setView(miPosGeo, 14);
   });
 
 </script>
